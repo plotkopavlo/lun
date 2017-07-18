05e0363420069cd9ca83c7a2062f53b4ef13ee0d
KievDev <kiev.devel@gmail.com>
2017-07-18 04:22:00 +0300

    removed filterByCity cuz finally denormalized DB

--- app/Repositories/FlatRepository.php
+++ app/Repositories/FlatRepository.php
@@ -11,39 +11,6 @@ class FlatRepository extends Repository {
     }
 
     /**
-     *
-     * TODO: denormalize DB and rewrite without joins
-     * @param $cityID
-     * @return $this
-     */
-    public function filterByCity($cityID)
-    {
-        $this->applyCriteria();
-
-        /** bugs cuz group by and select count(*) */
-        $this->model = $this->model
-            ->select('flat.id')
-
-            ->join('building_flat as bf', 'bf.flat_id', '=', 'flat.id')
-            ->join('building as b',  'bf.building_id', '=', 'b.id')
-            ->join('residential_complex as rc', 'b.residential_complex_id', '=', 'rc.id')
-            ->join('city as c', 'c.id', '=', 'rc.city_id')
-            ->where('c.id', '=', $cityID)
-
-            //mysql 7.5+ only group by...TODO: rewrite this night code
-            //->groupBy('bf.id')
-            //->groupBy('b.id')
-            //->groupBy('rc.id')
-            //->groupBy('c.id')
-            //->groupBy('flat.id')
-
-            //->distinct('flat.id')
-        ;
-
-        return $this;
-    }
-
-    /**
      * Get the maximum rooms in flat
      * @return mixed
      */
